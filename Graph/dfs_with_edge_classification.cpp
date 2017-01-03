e554e51c7574d1843a0527d1116e9639361375c9
mayank-tripathik <mayanktripathik@gmail.com>
2017-01-03 15:38:32 +0530

    Update dfs_with_edge_classification.cpp

--- Graph/dfs_with_edge_classification.cpp
+++ Graph/dfs_with_edge_classification.cpp
@@ -3,25 +3,33 @@ using namespace std;
 
 vector<int> start;
 vector<int> finish;
+vector<int> parent;
 int counter=0;
 
 void dfs(vector<list<int> > &graph, int u, vector<bool> &visited)
 {
-	cout<<"visited:"<<u<<endl;
+    cout<<"visited:"<<u<<endl;
 	visited[u]=true;
-	counter++;
-	start[u]=counter;
+    counter++;
+    start[u]=counter;
 	list<int>::iterator itr;
 	for(itr=graph[u].begin();itr!=graph[u].end();itr++)
-	{
+    {
 		int v=(*itr);
 		if(!visited[v])
-		{
-			dfs(graph,v,visited);
-		}
+        {
+            parent[v]=u;
+            dfs(graph,v,visited);
+        }       
 	}
-	counter++;
-	finish[u]=counter;
+    counter++;
+    finish[u]=counter;
+}
+
+void print_dfs_tree(vector<list<int> > &graph){
+    int nodes=graph.size()-1;
+    for(int i=1;i<=nodes;i++)
+        cout<<"parent of "<<i<<":"<<parent[i]<<endl;
 }
 
 void print_edges(vector<list<int> > &graph){
@@ -34,7 +42,9 @@ void print_edges(vector<list<int> > &graph){
         {
             int v=(*itr);
             cout<<u<<"--->"<<v<<" edge is a ";
-            if(start[u]>start[v]){
+            if(parent[v]==u)
+                cout<<"Tree edge\n";
+            else if(start[u]>start[v]){
                 if(finish[u]>finish[v])
                     cout<<"Cross edge\n";
                 else
@@ -42,11 +52,7 @@ void print_edges(vector<list<int> > &graph){
                 
             }
             else{
-                if(finish[u]>finish[v])
-                    cout<<"Tree edge\n";
-                else
-                    cout<<"Forward edge\n";
-                
+                    cout<<"Forward edge\n";  
             }
         }
     }   
@@ -59,14 +65,16 @@ int main()
 	cin>>nodes>>edges;
 	vector<list<int> >graph(nodes+1);
 	vector<bool> visited(nodes+1,false);
-	start.resize(nodes+1);
-	finish.resize(nodes+1);
-	for(int i=1;i<=edges;i++){
-	cin>>u>>v;
-	graph[u].push_back(v);
-	}
-	cin>>s;
-	dfs(graph,s,visited);
-	print_edges(graph);
-	return 0;
+    start.resize(nodes+1);
+    finish.resize(nodes+1);
+    parent.resize(nodes+1);
+    for(int i=1;i<=edges;i++){
+        cin>>u>>v;
+        graph[u].push_back(v);
+    }
+    cin>>s;
+    dfs(graph,s,visited);
+    print_dfs_tree(graph);
+    print_edges(graph);
+    return 0;
 }
