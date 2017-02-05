a353345a57949d7effb65ffdfa15ee3600dd194f
Mayank Kumar Tripathi <mayanktripathik@gmail.com>
2017-02-05 16:05:50 +0530

    Create revserse_linked_list_recursive.cpp

---
+++ Linked List/revserse_linked_list_recursive.cpp
@@ -0,0 +1,17 @@
+*
+ * Given a linked list. Reverse it.
+ * O(n) solution
+ */
+
+Node* Reverse(Node *head)
+{
+  if(head==NULL || head->next==NULL)
+      return head;
+    else
+    {
+        Node *current=Reverse(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return current;
+    }
+}
