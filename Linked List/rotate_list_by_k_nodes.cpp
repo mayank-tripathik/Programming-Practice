1c03d3d84d4871cb1edc545b0d4902ba521a6d7f
Mayank Kumar Tripathi <mayanktripathik@gmail.com>
2017-03-01 00:23:46 +0530

    Update rotate_list_by_k_nodes.cpp

--- Linked List/rotate_list_by_k_nodes.cpp
+++ Linked List/rotate_list_by_k_nodes.cpp
@@ -5,16 +5,6 @@
  */
  
 
-
-/*
- * Input:
- *  First List: 5->6->3  // represents number 365
- *  Second List: 8->4->2 //  represents number 248
- * Output
- *  Resultant list: 3->1->6  // represents number 613
- */
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
